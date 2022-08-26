24efa458b01f1623b443c3083eaf0cce2105d421
aemerchan <angel.merchan@zimait.com>
2022-08-26 06:35:45 -0500

    fin seccion 10 flutter chat service

--- flutter_chat_server/sockets/socket.js
+++ flutter_chat_server/sockets/socket.js
@@ -1,11 +1,33 @@
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require('../controllers/socket');
 
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
     
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token'] );
+
+    // verificar autenticacion
+    if (!valido) return client.disconnect();
+
+    // Cliente autenticado
+    usuarioConectado(uid);
+    
+    // Ingresar al usuario a ua sala en particular
+    // sala global, client.id, asdjfa2323jl323rj34388988
+    client.join(uid);
+
+    // Esuchar del cliente el mensaje-personal
+    client.on('mensaje-personal', async (payload) => {
+        
+        await grabarMensaje( payload );
+        io.to(payload.para).emit('mensaje-personal', payload);
+    });
+
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     // client.on('mensaje', (payload) => {
